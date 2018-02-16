96f0c65c693b3a26fdeb3d893fcc5973627a2f65
jsteele025 <jsteele025@yahoo.com>
2018-02-16 18:12:56 -0500

    message

---
+++ keys.js
@@ -0,0 +1,8 @@
+// console.log('this is loaded');
+
+exports.twitterKeys = {
+  consumer_key: 'Bj8MzJTHwqfouq25dzWSWlX4L',
+  consumer_secret: '5KpctAfDK8OcvB1dQYWwK71deGQ6c7B0MKH6daiAI7DFHj4rop',
+  access_token_key: '499082256903876610-BFeWj2aYoOADXqiw0EHovigCjCATGPG',
+  access_token_secret: 'DEJJj0JhUJdehXWwHwIO12ynI4YIf45xKmtTDkArSx29A',
+}
